838eb5d6f912c157145bff44e751ac6ce2b13e63
soeun06 <ivmt1126@gmail.com>
2022-03-22 21:15:07 +0900

    mission4

---
+++ src/main/java/dev/aquashdw/community/TestController.java
@@ -0,0 +1,19 @@
+package dev.aquashdw.community;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("test")
+    public @ResponseBody String test(){
+        return "Hello world!";
+    }
+}
